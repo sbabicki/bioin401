5b20bcbf71c5c578fb357632770a028cf2918546
Sasha Babicki <sbabicki@gmail.com>
2015-03-15 00:01:18 -0600

    first commit

---
+++ server.R
@@ -0,0 +1,180 @@
+library(shiny)
+library(gplots)
+setwd("~/Desktop/Dropbox/Winter 2015/Bioin 401/8_Mar17Presentation")
+# ~/Desktop/Dropbox/Winter 2015/Bioin 401/8_Mar17Presentation/program
+# runApp("program")
+
+shinyServer(function(input, output,session) {
+  
+  ########################################### FUNCTIONS ###########################################
+  
+  ################# get_file ################# 
+  # retrieves the file input data
+  get_file<-reactive({
+   
+    # input$file1 is NULL before upload
+    inFile <- input$file1
+    
+    if (is.null(inFile)){
+      return(NULL)
+    }
+    
+    # file has been uploaded
+    # data frame with 'name', 'size', 'type', and 'datapath' columns. 
+    # 'datapath' column contains the local filenames where the data can be found.
+    else{  
+      x <- read.csv(inFile$datapath, header=TRUE, sep=input$sep)
+      y <- remove_strings(x)
+    }
+    
+    if (is.null(y)){
+      return(NULL)
+    }
+    
+    # return the non empty file
+    else{
+      return(y)
+    }
+  })
+  
+  ################# remove_strings ################# 
+  remove_strings<-function(x){
+    nums <- sapply(x, is.numeric)
+    y<- x[,nums]
+    
+    nonNums <- !sapply(x, is.numeric)
+    omittedCols <- x[,nonNums]
+    
+    # try to find a column with title name
+    name = 'NAME'
+    tryCatch({
+      nameRow<-x[,name]
+      rownames(y) <- make.names(nameRow, unique=TRUE)
+      }, 
+      finally = {return(y)})
+  }
+  
+  ################# get_data_matrix ################# 
+  # returns file input data as a data matrix
+  get_data_matrix<-function(){
+    
+    # get the file data
+    fileData <- get_file()
+    if(is.null(fileData)){
+      return(NULL)
+    }
+    
+    # convert to data matrix
+    return(data.matrix(fileData))
+  }
+  
+  ################# data_analysis #################
+  data_analysis <- function(x){
+    x
+  }
+  
+  ################# get_colv #################
+  get_colv <- function(){
+    if(input$clusterMethod == 'none')
+      FALSE
+    else
+      input$colv
+  }
+  
+  ################# get_rowv #################
+  get_rowv <- function(){
+    if(input$clusterMethod == 'none')
+      FALSE
+    else
+      input$rowv
+  }
+  
+  ################# get_dendrogram ################# 
+  get_dendrogram <- function(){
+    if(input$dendCol && input$dendRow)
+      "both"
+    else if(input$dendCol)
+      "column"
+    else if(input$dendRow)
+      "row"
+    else
+      "none"
+  }
+  
+  ################# get_heatmap ################# 
+  # returns a heatmap created from the file input data
+  get_heatmap<-function(){
+    
+    # get the data matrix to convert to heatmap
+    heatmapDataMatrix <- get_data_matrix()
+    heatmapDataMatrix <- data_analysis(heatmapDataMatrix)
+    colv <- get_colv()
+    rowv <- get_rowv()
+    dendrogram <- get_dendrogram()
+    
+    if(is.null(heatmapDataMatrix))
+      return(NULL)
+    # customize colors
+    my_palette <- colorRampPalette(c(input$startColour, "black", input$endColour))(n = input$binSlider)
+    
+    # create the heatmap
+    heatmap.2(heatmapDataMatrix,
+              col=my_palette, scale=input$scale,
+              key=FALSE, symkey=FALSE, density.info="none", trace="none", 
+              Rowv = rowv, Colv = colv, dendrogram = dendrogram, 
+              hclustfun=function(c){
+                if(input$clusterMethod == "none")
+                  return("none")
+                else
+                  return(hclust(c, method=input$clusterMethod))
+                }, 
+              keysize=0.5, cexRow=1, 
+              main=input$imageTitle, xlab=input$xaxis, ylab=input$yaxis
+              #lmat = rbind(c(0,3),c(2,1),c(0,4)),
+              #lwid = c(1.5,4),
+              #lhei = c(1.5,4,1))
+              )
+
+      dev.off()
+  }
+
+  ################# get_height ################# 
+  get_height<-function(cond){
+    x<-get_data_matrix()
+    if(is.null(x))
+      return(0)
+    else{
+      if(cond == 1)
+        size <- nrow(x)*ncol(x)
+      else
+        size <- input$heightSlider
+      return(size)
+    }
+  }
+
+  ########################################### OUTPUT ###########################################
+  
+  ################# Table ################# 
+  output$dataTable <- renderTable({
+      get_file() 
+  })
+
+  ################# Heatmap ################# 
+  output$heatmap <- renderPlot({
+      get_heatmap()
+    }, height=reactive({get_height(input$previewFullSize)}))
+
+  ################# Save File ################# 
+  output$downloadData <- downloadHandler(
+    
+    filename = "heatmapNew",
+    
+    content = function(file) {
+        png(file,width=800, height=get_height(input$downloadFullSize))
+      
+        heatmapData <- get_heatmap()  
+      
+        dev.off()
+    }, 
+    contentType="image/png")
+  })
---
+++ ui.R
@@ -0,0 +1,138 @@
+# ui.R
+
+shinyUI(navbarPage(position = "fixed-top", theme = "bootstrap.css",
+  title = "Heatmap App",
+
+  #################### Analysis Tab ####################
+  tabPanel("Analysis",  
+           
+    ##########  Heatmap Plot Output ##########        
+    conditionalPanel(condition = "input.display == 'heatmap'",
+      absolutePanel(id="heatmapPanel",
+                    top = 0, left = 0, width="100%",
+                    draggable = TRUE,
+                    fixed = FALSE,
+                    plotOutput(outputId='heatmap', width= "100%"))),
+    
+    ##########  Options Menu ##########   
+    absolutePanel(style = "opacity: 0.92", id="dropdownMenu", 
+      top = 60, left = 0, width="100%", 
+      draggable = FALSE,
+      fixed = TRUE,
+    
+      ########## Option Menu Button ##########  
+      column(3,actionButton("showOptionsButton", label = h4("Options Menu")),
+    
+      conditionalPanel(condition = "input.showOptionsButton%2 == 0",   
+          ########## File input options ##########  
+            h3("File Input:"),
+            fileInput('file1',
+                      'Choose CSV File',
+                      accept=c('text/csv','text/comma-separated-values,text/plain','.csv')),
+            tags$textarea(id="textInput", rows=5, cols=25, ""),
+            
+            radioButtons('sep', 
+                     'File separator',
+                     c(Tab='\t', Comma=',', Semicolon=';'),
+                     '\t'))),
+          
+      
+        # needed to repeat because of col(3) end bracket
+        conditionalPanel(condition = "input.showOptionsButton%2 == 0",  
+          column(3, 
+            h3("Analysis:"),
+            selectInput("clusterMethod", label = "Clustering Method", 
+                        choices = list( 
+                          "none" = 'none',
+                          "mcquitty" = 'mcquitty'), 
+                        selected = 'none'), 
+            
+            conditionalPanel(condition = "input.clusterMethod == \'mcquitty\'",
+              strong("Apply To"),
+              checkboxInput('rowv', 'Rows', FALSE),
+              checkboxInput('colv', 'Columns', FALSE)),
+            selectInput("scale", label = "Scale Type", 
+                        choices = list(
+                          "row" = 'row',
+                          "column" = 'column',
+                          "none"='none'),
+                        selected = 'row'), 
+            strong("Show Dendrogram"),
+            checkboxInput('dendRow', 'Rows', FALSE),
+            checkboxInput('dendCol', 'Columns', FALSE)
+            
+          ),  
+                 
+                 
+          ########## Customize options ##########
+          column(3,  
+            h3("Customize Image:"),
+            radioButtons('display', 'Display',
+                         c(Table='table', Heatmap='heatmap'),
+                         'heatmap'),
+            sliderInput("heightSlider",
+                        "Height",
+                        min = 600, max = 2000, value = 1000),
+            checkboxInput('previewFullSize', 'Preview Full Size', FALSE),
+            
+            selectInput("startColour", label = "Start Colour", 
+                        choices = list( 
+                          "red" = 'red',
+                          "orange" = 'orange',
+                          "yellow" = 'yellow',
+                          "green" = 'green', 
+                          "blue" = 'blue',
+                          "purple" = 'purple'), 
+                        selected = 1),
+            selectInput("endColour", label = "End Colour", 
+                        choices = list(
+                          "green" = 'green',
+                          "red" = 'red',
+                          "orange" = 'orange',
+                          "yellow" = 'yellow', 
+                          "blue" = 'blue',
+                          "purple" = 'purple'), 
+                        selected = 1),
+           
+            sliderInput("binSlider",
+                          "Number of colours",
+                           min = 3, max = 299, value = 160)
+        ),
+          
+          ########## Save options ##########  
+          column(3,
+            h3("Save Output:"),
+            
+            textInput("imageTitle", label = "Title", value = ""),
+            textInput("xaxis", label = "X Axis Label", value = ""),
+            textInput("yaxis", label = "Y Axis Label", value = ""),
+            checkboxInput('downloadFullSize', 'Download Full Size', TRUE),
+            radioButtons('downloadFormat', 
+                         'File Format',
+                         c("PNG"='png', "PDF"='pdf', "JPEG"='jpeg'),
+                         'png'),
+            
+            downloadButton('downloadData', 'Save Image')))),
+    
+    ########## Figures output ##########
+    conditionalPanel(condition = "input.display == 'table'",
+      tableOutput('dataTable'))
+    
+    
+  ),
+  
+  
+  #################### Gallery Tab ####################
+  tabPanel("Gallery"), 
+  
+  #################### Instructions Tab ####################
+  tabPanel("Instructions",
+    h3("Instructions"),
+    wellPanel(
+    helpText("Tutiorial on how to use app"))),
+  
+  #################### Contact Tab ####################
+  tabPanel("Contact", 
+    wellPanel(paste("Contact me by email: babicki@ualberta.ca"))) 
+  
+))
